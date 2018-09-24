605213bf294bb235193d050f5e738c7c520afcda
sergio marelli <sergio.mrll@gmail.com>
2018-09-24 20:22:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-    delete object.[key]
-    return object
+    newobj = Object.assign({}, object)
+    delete newobj.key
+    return newobj
 }
